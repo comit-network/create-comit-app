9f73a4931ce20b904a268169040c2920c50d5e4a
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-09-11 17:02:16 +1000

    Refactor ethereum to use struct

--- src/ethereum.rs
+++ src/ethereum.rs
@@ -3,50 +3,57 @@ use web3::{
     api::Web3,
     futures::Future,
     transports::Http,
-    types::{Address, TransactionRequest, H256, U256},
+    types::{Address, TransactionRequest, U256},
 };
 
-pub fn start_ethereum_node() -> u32 {
-    let docker = testcontainers::clients::Cli::default();
-    let container = docker.run(testcontainers::images::parity_parity::ParityEthereum::default());
-
-    let port = container.get_host_port(8545).unwrap();
-
-    port
+pub struct EthereumNode {
+    pub port: u32,
 }
 
-pub fn fund(port: u32, address: Address, value: U256) {
-    let endpoint = format!("http://localhost:{}", port);
+impl EthereumNode {
+    pub fn start() -> Self {
+        let docker = testcontainers::clients::Cli::default();
+        let container =
+            docker.run(testcontainers::images::parity_parity::ParityEthereum::default());
 
-    let (_event_loop, transport) = Http::new(&endpoint).unwrap();
-    let client = Web3::new(transport);
+        EthereumNode {
+            port: container.get_host_port(8545).unwrap(),
+        }
+    }
 
-    let parity_dev_account: web3::types::Address =
-        "00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap();
+    pub fn fund(&self, address: Address, value: U256) {
+        let endpoint = format!("http://localhost:{}", &self.port);
 
-    // FIXME: Derive address from seed
-    // let taker_address: web3::types::Address =
-    //     "458968726a444a90fda1edc082129c661d39c7ff".parse().unwrap();
+        let (_event_loop, transport) = Http::new(&endpoint).unwrap();
+        let client = Web3::new(transport);
 
-    // U256::from_dec_str("200000000000000000000").unwrap();
+        let parity_dev_account: web3::types::Address =
+            "00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap();
 
-    client
-        .personal()
-        .send_transaction(
-            TransactionRequest {
-                from: parity_dev_account,
-                to: Some(address),
-                gas: None,
-                gas_price: None,
-                value: Some(value),
-                data: None,
-                nonce: None,
-                condition: None,
-            },
-            "",
-        )
-        .wait()
-        .unwrap();
+        // FIXME: Derive address from seed
+        // let taker_address: web3::types::Address =
+        //     "458968726a444a90fda1edc082129c661d39c7ff".parse().unwrap();
+
+        // U256::from_dec_str("200000000000000000000").unwrap();
+
+        client
+            .personal()
+            .send_transaction(
+                TransactionRequest {
+                    from: parity_dev_account,
+                    to: Some(address),
+                    gas: None,
+                    gas_price: None,
+                    value: Some(value),
+                    data: None,
+                    nonce: None,
+                    condition: None,
+                },
+                "",
+            )
+            .wait()
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
@@ -58,9 +65,9 @@ mod tests {
 
     #[test]
     fn got_port() {
-        let port = start_ethereum_node();
+        let ethereum = EthereumNode::start();
 
-        let endpoint = format!("http://localhost:{}", port);
+        let endpoint = format!("http://localhost:{}", ethereum.port);
         let (_event_loop, transport) = Http::new(&endpoint).unwrap();
         let client = Web3::new(transport);
 
@@ -74,11 +81,11 @@ mod tests {
     #[test]
     fn can_fund() {
         fn prop(address: Quickcheck<Address>, value: Quickcheck<U256>) -> bool {
-            let port = start_ethereum_node();
+            let ethereum = EthereumNode::start();
 
-            fund(port, address.clone().into(), value.clone().into());
+            ethereum.fund(address.clone().into(), value.clone().into());
 
-            let endpoint = format!("http://localhost:{}", port);
+            let endpoint = format!("http://localhost:{}", ethereum.port);
             let (_event_loop, transport) = Http::new(&endpoint).unwrap();
             let client = Web3::new(transport);
 
