db1ca9d48f4c1bcb81cc0d3f25158fabb8912f3e
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-09-11 17:43:06 +1000

    Can ping Bitcoin node

--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,36 +1,63 @@
 use bitcoincore_rpc::RpcApi;
-use rust_bitcoin::{self, Amount};
-use std::str::FromStr;
 use testcontainers::{self, Docker};
 
-pub fn start_bitcoin_node() {
-    let docker = testcontainers::clients::Cli::default();
-    let container = docker.run(testcontainers::images::coblox_bitcoincore::BitcoinCore::default());
-
-    let port = container.get_host_port(18443).unwrap();
-    let auth = container.image().auth();
-
-    let endpoint = format!("http://localhost:{}", port);
-
-    let client = bitcoincore_rpc::Client::new(
-        endpoint,
-        bitcoincore_rpc::Auth::UserPass(auth.username().to_owned(), auth.password().to_owned()),
-    )
-    .unwrap();
-
-    client.generate(101, None).unwrap();
-
-    // Fund maker address
-
-    // FIXME: Derive address from seed
-    let maker_address =
-        rust_bitcoin::Address::from_str("bcrt1qmmpq3reyxf7866xk57lvqemguhsjwq06h6x9pg").unwrap();
-    let amount = Amount::from_sat(100_000_000);
-
-    let _ = client.send_to_address(&maker_address, amount, None, None, None, None, None, None);
-
-    client.generate(1, None).unwrap();
+pub struct BitcoinNode {
+    pub port: u32,
+    pub auth: Auth,
 }
+
+impl BitcoinNode {
+    pub fn start() -> BitcoinNode {
+        let docker = testcontainers::clients::Cli::default();
+        let container =
+            docker.run(testcontainers::images::coblox_bitcoincore::BitcoinCore::default());
+        let auth = container.image().auth();
+
+        let node = BitcoinNode {
+            port: container.get_host_port(18443).unwrap(),
+            auth: Auth {
+                username: auth.username().to_string(),
+                password: auth.password().to_string(),
+            },
+        };
+
+        let endpoint = format!("http://localhost:{}", node.port);
+
+        let client = bitcoincore_rpc::Client::new(
+            endpoint,
+            bitcoincore_rpc::Auth::UserPass(auth.clone().username, auth.clone().password),
+        )
+        .unwrap();
+
+        client.generate(101, None).unwrap();
+
+        node
+    }
+}
+
+pub struct Auth {
+    username: String,
+    password: String,
+}
+
+impl From<Auth> for bitcoincore_rpc::Auth {
+    fn from(source: Auth) -> bitcoincore_rpc::Auth {
+        bitcoincore_rpc::Auth::UserPass(source.username, source.password)
+    }
+}
+
+// pub fn start_bitcoin_node() {
+//     // Fund maker address
+
+//     // FIXME: Derive address from seed
+//     let maker_address =
+//         rust_bitcoin::Address::from_str("bcrt1qmmpq3reyxf7866xk57lvqemguhsjwq06h6x9pg").unwrap();
+//     let amount = Amount::from_sat(100_000_000);
+
+//     let _ = client.send_to_address(&maker_address, amount, None, None, None, None, None, None);
+
+//     client.generate(1, None).unwrap();
+// }
 
 #[cfg(test)]
 mod tests {
@@ -41,7 +68,7 @@ mod tests {
         let bitcoin = BitcoinNode::start();
 
         let endpoint = format!("http://localhost:{}", bitcoin.port);
-        let client = bitcoincore_rpc::Client::new(endpoint, bitcoin.auth).unwrap();
+        let client = bitcoincore_rpc::Client::new(endpoint, bitcoin.auth.into()).unwrap();
 
         assert!(client.ping().is_ok())
     }
