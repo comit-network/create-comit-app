2556e7942a660167a3c5e05ced2fc71cc3f39694
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-09-11 18:18:22 +1000

    Can fund Bitcoin address

--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,5 +1,5 @@
 use bitcoincore_rpc::RpcApi;
-use rust_bitcoin::{Address, Amount};
+use rust_bitcoin::{hashes::sha256d, Address, Amount};
 use testcontainers::{self, Docker};
 
 pub struct BitcoinNode {
@@ -23,19 +23,28 @@ impl BitcoinNode {
         };
 
         let endpoint = format!("http://localhost:{}", node.port);
-
-        let client = bitcoincore_rpc::Client::new(
-            endpoint,
-            bitcoincore_rpc::Auth::UserPass(auth.clone().username, auth.clone().password),
-        )
-        .unwrap();
+        let client = bitcoincore_rpc::Client::new(endpoint, node.auth.clone().into()).unwrap();
 
         client.generate(101, None).unwrap();
 
         node
     }
+
+    pub fn fund(&self, address: &Address, amount: Amount) -> sha256d::Hash {
+        let endpoint = format!("http://localhost:{}", self.port);
+        let client = bitcoincore_rpc::Client::new(endpoint, self.auth.clone().into()).unwrap();
+
+        let transaction_id = client
+            .send_to_address(&address, amount, None, None, None, None, None, None)
+            .unwrap();
+
+        client.generate(1, None).unwrap();
+
+        transaction_id
+    }
 }
 
+#[derive(Clone)]
 pub struct Auth {
     username: String,
     password: String,
@@ -47,22 +56,45 @@ impl From<Auth> for bitcoincore_rpc::Auth {
     }
 }
 
-// pub fn start_bitcoin_node() {
-//     // Fund maker address
-
-//     // FIXME: Derive address from seed
-//     let maker_address =
-//         rust_bitcoin::Address::from_str("bcrt1qmmpq3reyxf7866xk57lvqemguhsjwq06h6x9pg").unwrap();
-//     let amount = Amount::from_sat(100_000_000);
-
-//     let _ = client.send_to_address(&maker_address, amount, None, None, None, None, None, None);
-
-//     client.generate(1, None).unwrap();
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rust_bitcoin::{Address, TxOut};
+    use std::convert::TryFrom;
+
+    trait FindUtxo {
+        fn find_utxo_at_transaction_for_address(
+            &self,
+            transaction_id: &sha256d::Hash,
+            address: &Address,
+        ) -> Option<TxOut>;
+    }
+
+    // Copied from blockchain_contracts tests
+    impl<Rpc: bitcoincore_rpc::RpcApi> FindUtxo for Rpc {
+        fn find_utxo_at_transaction_for_address(
+            &self,
+            transaction_id: &sha256d::Hash,
+            address: &Address,
+        ) -> Option<TxOut> {
+            let address = address.clone();
+            let unspent = self
+                .list_unspent(Some(1), None, Some(&[address]), None, None)
+                .unwrap();
+
+            #[allow(clippy::cast_sign_loss)] // it is just for the tests
+            unspent
+                .into_iter()
+                .find(|utxo| utxo.txid == *transaction_id)
+                .map(|result| {
+                    let value = u64::try_from(result.amount.as_sat()).unwrap();
+                    TxOut {
+                        value,
+                        script_pubkey: result.script_pub_key,
+                    }
+                })
+        }
+    }
 
     #[test]
     fn can_ping_bitcoin_node() {
@@ -77,15 +109,15 @@ mod tests {
     #[test]
     fn can_fund_bitcoin_address() {
         let bitcoin = BitcoinNode::start();
-        let address =
-            rust_bitcoin::Address::from_str("bcrt1qj4cuujh723tflnn7ypyq9uxrwf7cwm9kqrnmrk")
-                .unwrap();
-        let value = Amount::from_sat(1000);
-
-        let transaction_id = bitcoin.fund(address, value);
 
         let endpoint = format!("http://localhost:{}", bitcoin.port);
-        let client = bitcoincore_rpc::Client::new(endpoint, bitcoin.auth.into()).unwrap();
+        let client = bitcoincore_rpc::Client::new(endpoint, bitcoin.auth.clone().into()).unwrap();
+
+        let address = client.get_new_address(None, None).unwrap();
+
+        let value = Amount::from_sat(1_000);
+
+        let transaction_id = bitcoin.fund(&address, value);
 
         assert!(client
             .find_utxo_at_transaction_for_address(&transaction_id, &address)
