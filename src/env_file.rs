c5013562375019bce53f9f0af655c2e0a5145c1a
Franck Royer <franck@coblox.tech>
2019-09-20 18:03:19 +1000

    test: cannot lock if lock file is present

--- src/env_file.rs
+++ src/env_file.rs
@@ -2,9 +2,10 @@ use envfile::EnvFile;
 use std::fs::File;
 use std::io;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-const LOCK_FILE: &str = ".env.lock";
+const ENV_FILE: &str = ".env";
+const LOCK_FILE_SUFFIX: &str = ".lock";
 const LOCK_FILE_CONTENT: &[u8] =
     b"# This lock file is to ensure that no two create-comit-app updates the .env file.";
 
@@ -19,30 +20,39 @@ pub trait LockUpdateWrite {
     fn lock_update_write(&mut self, key: &str, value: &str) -> Result<(), Error>;
 }
 
-fn lock() -> Result<(), Error> {
-    let lock_path = Path::new(LOCK_FILE);
+fn lock_path(envfile: &EnvFile) -> PathBuf {
+    // TODO: Remove unwrap
+    let mut lock_path = envfile.path.as_os_str().to_str().unwrap().to_string();
+    lock_path.push_str(LOCK_FILE_SUFFIX);
+    Path::new(&lock_path).to_path_buf()
+}
+
+fn lock(envfile: &EnvFile) -> Result<(), Error> {
+    let lock_path = lock_path(envfile);
+
     if lock_path.exists() {
         Err(Error::IsLocked)
     } else {
-        let mut file = File::create(LOCK_FILE)?;
+        let mut file = File::create(lock_path)?;
         file.write_all(LOCK_FILE_CONTENT)?;
         Ok(())
     }
 }
 
-fn unlock() -> Result<(), Error> {
-    std::fs::remove_file(LOCK_FILE).map_err(|e| Error::CannotRemoveLock(e))
+fn unlock(envfile: &EnvFile) -> Result<(), Error> {
+    let lock_path = lock_path(envfile);
+    std::fs::remove_file(lock_path).map_err(|e| Error::CannotRemoveLock(e))
 }
 
 impl LockUpdateWrite for EnvFile {
     fn lock_update_write(&mut self, key: &str, value: &str) -> Result<(), Error> {
-        lock()
+        lock(self)
             .and(
                 self.update(key, value)
                     .write()
                     .map_err(Error::CannotUpdateEnvFile),
             )
-            .and(unlock())
+            .and(unlock(self))
     }
 }
 
@@ -58,13 +68,25 @@ mod tests {
 
     #[test]
     fn can_lock_and_unlock_twice() {
-        let path = tempfile::Builder::new().tempfile().unwrap();
-        let mut env_file = EnvFile::new(path.path()).unwrap();
+        let temp_file = tempfile::Builder::new().suffix(".env").tempfile().unwrap();
+        let mut env_file = EnvFile::new(temp_file.path()).unwrap();
 
         let res = env_file.lock_update_write("key", "value");
         assert!(res.is_ok());
 
         let res = env_file.lock_update_write("key", "value");
         assert!(res.is_ok());
+    }
+
+    #[test]
+    fn cannot_lock() {
+        let temp_file = tempfile::Builder::new().suffix(".env").tempfile().unwrap();
+        let mut env_file = EnvFile::new(temp_file.path()).unwrap();
+
+        let mut lock_file = File::create(lock_path(&env_file)).unwrap();
+        lock_file.write_all(b"").unwrap();
+
+        let res = env_file.lock_update_write("key", "value");
+        assert!(res.is_err());
     }
 }
