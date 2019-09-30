d9ec24afe2969a393bffe3be892e4ee5e9bbcd61
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-09-30 15:03:37 +1000

    Do not do an initial commit
    Instead there will be a bunch of untracked files present.

--- src/new.rs
+++ src/new.rs
@@ -1,4 +1,4 @@
-use git2::{IndexAddOption, Repository, ResetType};
+use git2::Repository;
 use std::io::{Read, Write};
 
 const HELLO_SWAP_ZIP: &str = "https://github.com/comit-network/hello-swap/archive/master.zip";
@@ -20,26 +20,5 @@ pub fn new(name: String) {
         .expect("Could not unzip bundle");
 
     let _ = Repository::init(format!("./{}", name))
-        .map_err(|e| panic!("Failed to create an empty Git repository: {:?}", e))
-        .and_then(create_initial_commit)
-        .map_err(|e| panic!("Failed to create initial commit: {:?}", e));
-}
-
-fn create_initial_commit(repo: Repository) -> Result<(), git2::Error> {
-    let sig = repo.signature()?;
-
-    let tree_id = {
-        let mut index = repo.index()?;
-        index.add_all(Vec::<String>::new(), IndexAddOption::DEFAULT, None)?;
-        index.write_tree()?
-    };
-
-    let tree = repo.find_tree(tree_id)?;
-    let commit = repo.commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[])?;
-
-    // Otherwise the index is in a weird state after the commit.
-    let obj = repo.find_object(commit, None)?;
-    repo.reset(&obj, ResetType::Hard, None)?;
-
-    Ok(())
+        .map_err(|e| panic!("Failed to create an empty Git repository: {:?}", e));
 }
