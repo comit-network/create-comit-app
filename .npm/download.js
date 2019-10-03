d038753c3affb62e5cca8dc78944fe22e1d45d78
Franck Royer <franck@coblox.tech>
2019-10-03 21:12:02 +1000

    Fix download

--- .npm/download.js
+++ .npm/download.js
@@ -1,9 +1,9 @@
-const https = require('https');
 const fs = require('fs');
 const packageJson = require("./package");
-
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const unzipper = require("unzipper");
+const axios = require('axios');
 
 async function getSystem() {
   const {stdout} = await exec("uname -s");
@@ -15,6 +15,10 @@ async function getArch() {
   return stdout.trim();
 }
 
+function unzip(filepath) {
+  fs.createReadStream(filepath)
+    .pipe(unzipper.Extract({path: './'}));
+}
 
 (async () => {
   const version = packageJson.version;
@@ -24,21 +28,31 @@ async function getArch() {
     throw new Error("Could not retrieve needed information.");
   }
   const filename = `create-comit-app_${version}_${system}_${arch}.zip`;
+
+  if (fs.existsSync(filename)) {
+    fs.unlinkSync(filename);
+  }
+
   const url = `https://github.com/comit-network/create-comit-app/releases/download/${version}/${filename}`;
-  console.log(url);
   const file = fs.createWriteStream(filename);
 
-  https.get(url, (response) => {
-    const {statusCode} = response;
-
-    if (statusCode === 404) {
-      throw new Error("404 error received, your system may not be supported or GitHub is down.")
-    } else if (statusCode !== 200 && statusCode !== 302) {
-      throw new Error('Download Failed.\n' +
-        `Status Code: ${statusCode}`);
-    }
-
-    response.pipe(file);
+  let response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
   });
+
+  if (response.status === 302) {
+    console.log(response.headers.location);
+    response = await axios({
+      url: response.headers.location,
+      method: 'GET',
+      responseType: 'stream'
+    });
+  }
+
+  response.data.pipe(file);
+
+  // unzip(filename);
 })();
 
