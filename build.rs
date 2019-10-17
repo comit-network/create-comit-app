8969553020850807c9203ecd9cb662c0d272d698
Thomas Eizinger <thomas@eizinger.io>
2019-10-17 12:56:17 +1100

    Use anyhow instead of snafu

--- build.rs
+++ build.rs
@@ -1,44 +1,28 @@
+use anyhow::Context;
 use flate2::{write::GzEncoder, Compression};
 use ignore::WalkBuilder;
-use snafu::ResultExt;
-use std::path::PathBuf;
-use std::{env, fs::File, io, path::Path};
+use std::{env, fs::File, path::Path};
 
-#[derive(Debug, snafu::Snafu)]
-enum Error {
-    #[snafu(display("Unable to read OUT_DIR environment variable"))]
-    ResolveOutDir { source: env::VarError },
-    #[snafu(display("Unable to create temporary archive at {}", path.display()))]
-    CreateArchive { source: io::Error, path: PathBuf },
-    #[snafu(display("Unable to switch current working directory to {}", path.display()))]
-    SwitchWorkingDirectory { source: io::Error, path: PathBuf },
-    #[snafu(display("Unable to append {} to archive", path.display()))]
-    AppendToArchive { source: io::Error, path: PathBuf },
-    #[snafu(display("Failure while walking directory tree"))]
-    WalkDir { source: ignore::Error },
-}
-
-fn main() -> Result<(), Error> {
-    let out_dir = env::var("OUT_DIR").context(ResolveOutDir)?;
+fn main() -> anyhow::Result<()> {
+    let out_dir = env::var("OUT_DIR").context("unable to read OUT_DIR variable")?;
     let out_dir = Path::new(&out_dir);
     let archive = out_dir.join("new_project.tar.gz");
 
-    let archive = File::create(archive.clone()).context(CreateArchive { path: archive })?;
+    let archive = File::create(archive.clone())
+        .with_context(|| format!("unable to create archive at {}", archive.display()))?;
     let mut archive = tar::Builder::new(GzEncoder::new(archive, Compression::default()));
 
     let new_project_folder = Path::new("./new_project");
-    env::set_current_dir(&new_project_folder).context(SwitchWorkingDirectory {
-        path: new_project_folder,
-    })?;
+    env::set_current_dir(&new_project_folder).context("unable to switch working directory")?;
 
     // use the ignore library to skip all files specified in .gitignore
     for result in WalkBuilder::new("./").hidden(false).build() {
-        let entry = result.context(WalkDir)?;
+        let entry = result.context("unable to walk directory")?;
         let path = entry.path();
 
         archive
             .append_path(path)
-            .context(AppendToArchive { path })?;
+            .with_context(|| format!("unable to add {} to the archive", path.display()))?;
         // prevent rerun if files did not change
         println!("cargo:rerun-if-changed={}", path.display());
     }
