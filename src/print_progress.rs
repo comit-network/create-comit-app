6036c5365fba44a76163073b08db7013074e1297
Franck Royer <franck@coblox.tech>
2019-10-28 12:41:17 +1100

    Import in macro to avoid surprises

--- src/print_progress.rs
+++ src/print_progress.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! print_progress {
     ($($arg:tt)*) => ({
+        use std::io::Write;
         print!($($arg)*);
         print!("...");
         std::io::stdout().flush().ok().expect("Could not flush stdout");
