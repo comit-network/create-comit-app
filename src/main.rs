de11e001ea68bf9ca6be1aeaf78c18f28fdba4f2
Thomas Eizinger <thomas@eizinger.io>
2019-12-30 17:33:01 +1100

    Make `new` command async
    This forces use to drop the compression of the archive that is
    included in the binary. Since we only include the source files,
    this archive is only 400kb big so no big deal.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-#![type_length_limit = "8753955"]
+#![type_length_limit = "8760995"]
 
 use create_comit_app::{create_comit_app::CreateComitApp, env, new::new};
 use structopt::StructOpt;
-
-const NEW_PROJECT_ARCHIVE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/new_project.tar.gz"));
 
 fn main() -> std::io::Result<()> {
     let mut runtime = tokio_compat::runtime::Runtime::new()?;
@@ -18,7 +16,7 @@ fn main() -> std::io::Result<()> {
 async fn run_command(command: CreateComitApp) -> std::io::Result<()> {
     match command {
         CreateComitApp::StartEnv => env::start().await,
-        CreateComitApp::New { name } => new(name, NEW_PROJECT_ARCHIVE)?,
+        CreateComitApp::New { name } => new(name).await?,
         CreateComitApp::ForceCleanEnv => env::clean_up().await,
     }
 
