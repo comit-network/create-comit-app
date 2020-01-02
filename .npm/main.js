5d46494f581d37125d30f498417aa92a35e6c9c7
Franck Royer <franck@coblox.tech>
2020-01-03 09:17:30 +1100

    Explain regex

--- .npm/main.js
+++ .npm/main.js
@@ -4,6 +4,10 @@ const download = require("./download");
 const execute = require("./execute");
 
 async function main(dirPath, args) {
+  // Sometimes the only way to test is to publish on npmjs.com, In this case,
+  // it is easiest to change the version in package.json to X.Y.ZrcN,
+  // with X.Y.Z the version of create-comit-app. The regex below removes
+  // the "rcN" suffix to download the binary.
   const ccaVersion = /^\d\.\d\.\d/.exec(packageJson.version)[0];
   const binPath = `${dirPath}/create-comit-app_${ccaVersion}/create-comit-app`;
 
