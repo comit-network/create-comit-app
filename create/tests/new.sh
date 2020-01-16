43d4a66b5fb920b979056205c1148846c2d6e244
Franck Royer <franck@coblox.tech>
2020-01-16 14:13:13 +1100

    ls and cat if failure

--- create/tests/new.sh
+++ create/tests/new.sh
@@ -14,7 +14,7 @@ function clean () {
 ## Start tests
 echo "Running $0"
 
-$CCA "${PROJECT_NAME}" > /dev/null || (echo "FAIL: Non-zero exit code returned."; clean ; exit 1;)
+$CCA "${PROJECT_NAME}" > /dev/null || (echo "FAIL: Non-zero exit code returned."; clean; exit 1;)
 
 test -d "${PROJECT_NAME}" > /dev/null || (echo "FAIL: Project directory ${PROJECT_NAME} was not created."; clean; exit 1;)
 
@@ -24,6 +24,8 @@ PACKAGE_JSON_PROJECT_NAME=$(cat "${PROJECT_NAME}/package.json" | jq .name)
 if [ "$PACKAGE_JSON_PROJECT_NAME" != "\"$PROJECT_NAME\"" ]
 then
   echo "FAIL: Project was not properly initialized with ${PROJECT_NAME} in package.json."
+  ls
+  cat "${PROJECT_NAME}/package.json"
   clean
   exit 1
 fi
